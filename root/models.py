8f15fc7331ab931edfb3b52413efb7cfaf2051ee
reza mohseni <155166979+rezaamohseni@users.noreply.github.com>
2024-05-22 00:17:25 +0330

    Update models.py

--- root/models.py
+++ root/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Team(models.Model):
+    image = models.ImageField()
+    user = models.ForeignKey()
+    
 # Create your models here.
