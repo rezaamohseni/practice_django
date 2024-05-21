829e31ff34a6324bea24a85fa7cb6f228541ca89
reza mohseni <155166979+rezaamohseni@users.noreply.github.com>
2024-05-21 17:48:52 +0330

    base.html created

--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path
-from .views import *
+from .views import home
 urlpatterns = [
     path('' , home)
 ]
